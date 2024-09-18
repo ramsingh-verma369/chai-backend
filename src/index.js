e59c5c440adeb4a3f503bb233327b42c8256e1a4
ramsingh-verma369 <ramsinghverma9450@gmail.com>
2024-09-18 06:59:44 +0530

    "some utils file completed"

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
-import express from 'express'
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
 
 dotenv.config();
 
-connectDB();
+const PORT = process.env.PORT || 8000;
+
+connectDB()
+.then(() => {
+    app.listen(PORT,() => {
+        console.log(`Server is running on the ${PORT}`)
+    })
+
+    app.on("ERRR",(error) => {
+        console.log("Error",error);
+        throw error;
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection is failed",err);
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,26 @@
+// we can use requestHandler or fxn or whatever fnx_name
+const asyncHandler = (requestHandler) => {
+    (req,res,next) => {
+        Promise.resolve(requestHandler(req,res,next)).catch((err) => next(err))
+    }
+}
+
+export  {asyncHandler}
+
+
+// const asyncHandler = () => {}
+// const asyncHandler = (fn) => () => {}
+// const asyncHandler = (fn) => async () => {}
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req,res,next);
+//     } catch (err) {
+//         res.status(err.code || 500).json({
+//             success: true,
+//             message: err.message
+//         })
+//     }
+// }
+
+
