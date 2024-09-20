5033d846cd85bc3f4e4a2cfce2174bcb39cbb503
ramsingh-verma369 <ramsinghverma9450@gmail.com>
2024-09-20 08:19:20 +0530

    "remove some bugs in utils folder and user.router"

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,5 +1,7 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from 'fs';
+import dotenv from 'dotenv'
+dotenv.config()
 
 
 cloudinary.config({ 
@@ -17,6 +19,7 @@ const uploadOnCloudinary = async (localFilePath) => {
             resource_type: "auto"
         })
         console.log("file is uploaded successfully",response.url);
+        fs.unlinkSync(localFilePath);
         return response;
 
     } catch (error) {
