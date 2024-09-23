e5a25eadc98273450f51c45d4571b1a51d1f6c14
ramsingh-verma369 <ramsinghverma9450@gmail.com>
2024-09-23 10:54:33 +0530

    "user controller and authmiddleware are completed"

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/user.controller.js";
+import { registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"
 
 const router = Router();
@@ -16,8 +16,6 @@ router.route('/register').post(
         }
     ]),
     registerUser);
-
-router.route("/login").post(loginUser)
 
 
 
